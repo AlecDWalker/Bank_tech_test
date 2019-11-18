c696ff5b8a70d39e068f0593bca62a18100c9bd7
AlecDWalker <walkeralec@hotmail.com>
2019-11-18 13:20:56 +0000

    withdrawl, deposit and return balance/transaction methods added

--- lib/account.rb
+++ lib/account.rb
@@ -2,4 +2,26 @@ require 'pg'
 
 class Account
 
+  def initialize
+    @transactions = []
+  end
+
+  def return_transactions
+    @transactions
+  end
+
+  def return_balance
+    sum = 0
+    @transactions.each{ |t|
+    sum += t  }
+    return sum
+  end
+
+  def deposit(amount)
+    @transactions << amount
+  end
+
+  def withdrawl(amount)
+    @transactions << (-amount)
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,37 @@
 require 'account'
 
+
 describe Account do
-  
+  it 'starts with a balance of zero' do
+    account = Account.new
+    expect(account.return_balance).to eq 0
+  end
+
+  describe 'deposit' do
+    it 'can increase the balance' do
+      account = Account.new
+      account.deposit(500)
+      expect(account.return_balance).to eq 500
+    end
+  end
+
+  describe 'withdrawl' do
+    it 'can decrease the balance' do
+      account = Account.new
+      account.withdrawl(100)
+        expect(account.return_balance).to eq -100
+    end
+  end
+
+  describe 'transaction history' do
+    it 'can record mulitple transactions' do
+      account = Account.new
+      account.deposit(100)
+      account.deposit(600)
+      account.withdrawl(200)
+      expect(account.return_transactions.length). to eq 3
+      expect(account.return_balance).to eq 500
+    end
+  end
+
 end
